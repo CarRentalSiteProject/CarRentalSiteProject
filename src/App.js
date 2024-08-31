e17a9ee8d746036b3213e7d8f19c75616b84cd90
Feng <a28484038@gmail.com>
2024-08-31 20:46:48 +0800

    將登入頁面整合進去

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@popperjs/core';
-//import './styles/*';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
   const [error, setError] = useState(null);
-  const baseUrl = "http://localhost:8080/carrent/test_api";
+  const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:8080/api/login";
 
   useEffect(() => {
     axios.get(baseUrl)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(error => {
-      setError(error);
-    })
-  }, []);
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(error => {
+        setError(error);
+      });
+  }, [baseUrl]);
 
-  if (error) return `Error: ${error.message}`;
-  
+  if (error) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        Error: {error.message}
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Header />
-      <MainContent />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="login" element={<Login />} />
+          <Route path="index" element={<MainContent />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
